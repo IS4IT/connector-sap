d697654994bba61109db7e9f8530b7ab76afd585
Lothar Haeger <lothar.haeger@is4it.de>
2026-06-09 17:19:53 +0200

    Add IS4IT copyright notices
    Attribute the work done on top of the Evolveum connector to IS4IT:
    - files added by IS4IT carry an IS4IT-only header (ReadTableStructure,
      SapTableParsingTest, SapSubTableParsingTest, SapReadTableLiveTest)
    - pre-existing Evolveum files substantially changed by IS4IT get an IS4IT line
      below Evolveum's (SapConnector, SapConfiguration)
    - TestClient, which had no header, gets one crediting its original Evolveum
      committers (2016, 2019) and IS4IT (2026)
    
    License is unchanged (Apache 2.0); only attribution lines are added.

--- src/main/java/com/evolveum/polygon/connector/sap/SapConnector.java
+++ src/main/java/com/evolveum/polygon/connector/sap/SapConnector.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2010-2016 Evolveum
+ * Copyright (c) 2026 IS4IT
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
